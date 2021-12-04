fdcc19c5b3c422cefcfca40ef2cde03946e9a7b8
Alec Thomas <alec@swapoff.org>
2021-12-04 13:17:33 +1100

    Initial commit.

---
+++ atomic_test.go
@@ -0,0 +1,67 @@
+package atomic_test
+
+import (
+	"fmt"
+	"testing"
+
+	stdlibatomic "sync/atomic"
+
+	"github.com/alecthomas/assert/v2"
+	"github.com/alecthomas/atomic"
+)
+
+func TestAtomic(t *testing.T) {
+	v := atomic.New("hello")
+	assert.Equal(t, "hello", v.Load())
+	v.Store("world")
+	assert.Equal(t, "world", v.Load())
+}
+
+func TestInt32(t *testing.T) {
+	v := atomic.NewInt32(0)
+	assert.Equal(t, 0, v.Load())
+	assert.Equal(t, true, v.CompareAndSwap(0, 10))
+	assert.Equal(t, false, v.CompareAndSwap(0, 10))
+}
+
+func BenchmarkInt64Add(b *testing.B) {
+	v := atomic.NewInt64(0)
+	for i := 0; i < b.N; i++ {
+		v.Add(1)
+	}
+}
+
+func BenchmarkIntInterfaceAdd(b *testing.B) {
+	var v atomic.Int[int64] = atomic.NewInt64(0)
+	for i := 0; i < b.N; i++ {
+		v.Add(1)
+	}
+}
+
+func BenchmarkStdlibInt64Add(b *testing.B) {
+	var n int64
+	for i := 0; i < b.N; i++ {
+		stdlibatomic.AddInt64(&n, 1)
+	}
+}
+
+func BenchmarkInterfaceStore(b *testing.B) {
+	var v atomic.Interface[string] = atomic.New("hello")
+	for i := 0; i < b.N; i++ {
+		v.Store(fmt.Sprint(i))
+	}
+}
+
+func BenchmarkValueStore(b *testing.B) {
+	v := atomic.New("hello")
+	for i := 0; i < b.N; i++ {
+		v.Store(fmt.Sprint(i))
+	}
+}
+
+func BenchmarkStdlibValueStore(b *testing.B) {
+	v := stdlibatomic.Value{}
+	for i := 0; i < b.N; i++ {
+		v.Store(fmt.Sprint(i))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/alecthomas/atomic
+
+go 1.18
+
+require github.com/alecthomas/assert/v2 v2.0.0-alpha3
+
+require (
+	github.com/alecthomas/repr v0.0.0-20210801044451-80ca428c5142 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/hexops/gotextdiff v1.0.3 // indirect
+)
